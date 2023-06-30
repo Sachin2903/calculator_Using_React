05131d19024017c8cd2cfa218897b483c0ab6436
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-07-01 01:53:06 +0530

    adde cal

--- src/calculator.js
+++ src/calculator.js
@@ -1,15 +1,20 @@
 import styles from "./sass/mainSass.module.css";
-import {Fragment} from "react";
-export function Calculator(){
-    return(
+import { Fragment } from "react";
+export function Calculator() {
+    return (
         <Fragment>
             <h1>result</h1>
             <div className={styles.calcu}>
-
+                <input className={styles.inputs}  type="number" />
+                <input className={styles.inputs} type="number" />
+                <button className={styles.btn}>+</button>
+                <button className={styles.btn}>-</button>
+                <button className={styles.btn}>&times;</button>
+                <button className={styles.btn}>/</button>
             </div>
 
         </Fragment>
 
-       
+
     )
 }
