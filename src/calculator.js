07b902c22b85a0a393333c1a40822bb1ffe9d0b9
Sachin2903 <sachin.18bme1170@abes.ac.in>
2023-07-01 01:30:20 +0530

    added structure

---
+++ src/calculator.js
@@ -0,0 +1,15 @@
+import styles from "./sass/mainSass.module.css";
+import {Fragment} from "react";
+export function Calculator(){
+    return(
+        <Fragment>
+            <h1>result</h1>
+            <div className={styles.calcu}>
+
+            </div>
+
+        </Fragment>
+
+       
+    )
+}
